4590650c9268f418adf7bc94ebbd565b00e27abc
unknown <jerrardsmith215@gmail.com>
2019-03-09 12:39:49 -0500

    Routes staged. Starting CRUD operations on 'actions'

---
+++ data/routers/projects.js
@@ -0,0 +1,14 @@
+// intialize route
+const express = require('express');
+const router = express.Router();
+const db = require('../helpers/projectModel');
+
+/* CRUD OPERATIONS */
+
+// Create/post logic
+
+// Read/get logic
+
+// Update/put logic
+
+// Delete/remove logic
