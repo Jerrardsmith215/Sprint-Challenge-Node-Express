3924d1b5a0e5f2c9c4c0e9453996fefacdde82d8
unknown <jerrardsmith215@gmail.com>
2019-03-09 12:28:23 -0500

    Initial commit, server staged

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-// play this: https://www.youtube.com/watch?v=d-diB65scQU
+// initialize server and routes
+const express = require('express');
+const server = express();
+const projects = require('./data/routers/projects');
+const actions = require('./data/routers/actions');
 
-// code away!
+// server variables and middleware
+const port = 9090; 
+const parser = express.json();
+
+const cors = require('cors');
+const helmet = require('helmet');
+const logger = require('morgan');
+
+server.use(
+    parser,
+    logger('tiny'),
+    helmet(), 
+    cors(),
+    );
+
+// define routes and activate server
+server.use('/api/projects', projects);
+server.use('/api/actions', actions);
+
+server.listen(port, () => {
+    console.log(`Server Started on port ${port}`); 
+});
